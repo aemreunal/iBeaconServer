0082b33214461ac5d5dcdde51d27830691d8f114
A. Emre Ünal <aemreunal@gmail.com>
2014-07-18 16:06:07 +0300

    Initial commit

---
+++ src/main/java/com/dteknoloji/Test.java
@@ -0,0 +1,14 @@
+package com.dteknoloji;
+
+/*
+ * This code belongs to:
+ * Ahmet Emre Unal
+ * S001974
+ * emre.unal@ozu.edu.tr
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
