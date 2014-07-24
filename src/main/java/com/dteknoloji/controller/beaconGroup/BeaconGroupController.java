44c3e596a62aa47ea338db55641567158636def5
A. Emre Ünal <aemreunal@gmail.com>
2014-07-24 14:19:12 +0300

    Delete erronous method

--- src/main/java/com/dteknoloji/controller/beaconGroup/BeaconGroupController.java
+++ src/main/java/com/dteknoloji/controller/beaconGroup/BeaconGroupController.java
@@ -62,25 +62,6 @@ public class BeaconGroupController {
         }
     }
 
-
-    @RequestMapping(method = RequestMethod.POST,value = "/{id}/beacon")
-    public ResponseEntity<BeaconGroup> createBeaconWithGroup(@RequestBody BeaconGroup restBeaconGroup,@PathVariable String id, UriComponentsBuilder builder) {
-        try {
-            BeaconGroup newBeaconGroup = service.save(restBeaconGroup);
-            if (GlobalSettings.DEBUGGING) {
-                System.out.println("Saved beacon group with ID = \'" + newBeaconGroup.getBeaconGroupId() + "\' name = \'" + newBeaconGroup.getName() + "\'");
-            }
-            HttpHeaders headers = new HttpHeaders();
-            headers.setLocation(builder.path("/BeaconGroup/{id}").buildAndExpand(newBeaconGroup.getBeaconGroupId().toString()).toUri());
-            return new ResponseEntity<BeaconGroup>(newBeaconGroup, headers, HttpStatus.CREATED);
-        } catch (ConstraintViolationException | TransactionSystemException e) {
-            if (GlobalSettings.DEBUGGING) {
-                System.err.println("Unable to save beacon group! Constraint violation detected!");
-            }
-            return new ResponseEntity<BeaconGroup>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}", produces = "application/json")
     public ResponseEntity<BeaconGroup> deleteBeaconGroup(@PathVariable String id) {
 
