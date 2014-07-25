703e67f3a6dca08d3703cc7a2834c9e6bebb5a55
A. Emre Ünal <aemreunal@gmail.com>
2014-07-25 17:41:21 +0300

    Add project attribute

--- src/main/java/com/dteknoloji/domain/BeaconGroup.java
+++ src/main/java/com/dteknoloji/domain/BeaconGroup.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /*
@@ -108,6 +109,26 @@ public class BeaconGroup {
     }
     /*
      * END: Beacon group 'beacon list' attribute
+     *------------------------------------------------------------
+     */
+
+    /*
+     *------------------------------------------------------------
+     * BEGIN: Beacon group 'project' attribute
+     */
+    @ManyToOne(targetEntity = Project.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
+    @JsonIgnore
+    private Project project;
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+    /*
+     * END: Beacon group 'project' attribute
      *------------------------------------------------------------
      */
 }
