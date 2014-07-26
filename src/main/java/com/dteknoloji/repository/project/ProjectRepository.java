fbe8dd652ec86a58be32a7be2846770f50defd98
A. Emre Ünal <aemreunal@gmail.com>
2014-07-26 17:39:40 +0300

    Add Project repository

---
+++ src/main/java/com/dteknoloji/repository/project/ProjectRepository.java
@@ -0,0 +1,25 @@
+package com.dteknoloji.repository.project;
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import com.dteknoloji.domain.Project;
+
+/*
+ **************************
+ * Copyright (c) 2014     *
+ *                        *
+ * This code belongs to:  *
+ *                        *
+ * Ahmet Emre Ünal        *
+ * S001974                *
+ *                        *
+ * aemreunal@gmail.com    *
+ * emre.unal@ozu.edu.tr   *
+ *                        *
+ * aemreunal.com          *
+ **************************
+ */
+
+public interface ProjectRepository extends CrudRepository<Project, Long>, JpaSpecificationExecutor {
+
+}
