180887964b31a552c2a33509373ee0115f405789
A. Emre Ünal <aemreunal@gmail.com>
2014-08-04 15:23:27 +0300

    Disable importing beacons

--- src/main/java/com/aemreunal/controller/BeaconController.java
+++ src/main/java/com/aemreunal/controller/BeaconController.java
@@ -1,15 +1,13 @@
 package com.aemreunal.controller;
 
-import java.util.List;
-import javax.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.TransactionSystemException;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-import com.aemreunal.config.GlobalSettings;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import com.aemreunal.domain.Beacon;
 import com.aemreunal.service.BeaconService;
 
@@ -58,32 +56,32 @@ public class BeaconController {
         }
         return new ResponseEntity<Beacon>(beacon, HttpStatus.OK);
     }
-
-    /**
-     * Create a multiple new beacons
-     *
-     * @param restBeacons
-     *     The beacon list as JSON object
-     *
-     * @return The created beacons
-     */
-    @RequestMapping(method = RequestMethod.POST, value = "/import")
-    public ResponseEntity<List<Beacon>> createMultipleBeacon(@RequestBody List<Beacon> restBeacons) {
-        for (Beacon restBeacon : restBeacons) {
-            try {
-                Beacon newBeacon = beaconService.save(restBeacon);
-                if (GlobalSettings.DEBUGGING) {
-                    System.out.println("Saved beacon with UUID = \'" + newBeacon.getUuid() + "\' major = \'" + newBeacon.getMajor() + "\' minor = \'" + newBeacon.getMinor() + "\'");
-                }
-            } catch (ConstraintViolationException | TransactionSystemException e) {
-                if (GlobalSettings.DEBUGGING) {
-                    System.err.println("Unable to save beacon! Constraint violation detected!");
-                }
-                return new ResponseEntity<List<Beacon>>(HttpStatus.BAD_REQUEST);
-            }
-        }
-        return new ResponseEntity<List<Beacon>>(restBeacons, HttpStatus.CREATED);
-    }
+//
+//    /**
+//     * Create a multiple new beacons
+//     *
+//     * @param restBeacons
+//     *     The beacon list as JSON object
+//     *
+//     * @return The created beacons
+//     */
+//    @RequestMapping(method = RequestMethod.POST, value = "/import")
+//    public ResponseEntity<List<Beacon>> createMultipleBeacon(@RequestBody List<Beacon> restBeacons) {
+//        for (Beacon restBeacon : restBeacons) {
+//            try {
+//                Beacon newBeacon = beaconService.save(restBeacon);
+//                if (GlobalSettings.DEBUGGING) {
+//                    System.out.println("Saved beacon with UUID = \'" + newBeacon.getUuid() + "\' major = \'" + newBeacon.getMajor() + "\' minor = \'" + newBeacon.getMinor() + "\'");
+//                }
+//            } catch (ConstraintViolationException | TransactionSystemException e) {
+//                if (GlobalSettings.DEBUGGING) {
+//                    System.err.println("Unable to save beacon! Constraint violation detected!");
+//                }
+//                return new ResponseEntity<List<Beacon>>(HttpStatus.BAD_REQUEST);
+//            }
+//        }
+//        return new ResponseEntity<List<Beacon>>(restBeacons, HttpStatus.CREATED);
+//    }
 
     /**
      * Delete the specified beacon
