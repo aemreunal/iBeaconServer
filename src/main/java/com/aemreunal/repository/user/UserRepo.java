5b485164f77e47e97662b2d07ad8b6ebfa6f890a
A. Emre Ünal <aemreunal@gmail.com>
2014-08-13 12:05:14 +0300

    Add user repo

---
+++ src/main/java/com/aemreunal/repository/user/UserRepo.java
@@ -0,0 +1,25 @@
+package com.aemreunal.repository.user;
+
+/*
+ ***************************
+ * Copyright (c) 2014      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * aemreunal@gmail.com     *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import com.aemreunal.domain.User;
+
+public interface UserRepo extends CrudRepository<User, Long>, JpaSpecificationExecutor {
+//    Beacon findByBeaconIdAndProject(Long beaconId, Project project);
+}
