4b0d3415bba4f1ff325ad9524667f6f8ce1f6b00
A. Emre Ünal <aemreunal@gmail.com>
2014-08-14 20:56:47 +0300

    Add new exception

---
+++ src/main/java/com/aemreunal/exception/project/ProjectNotFoundException.java
@@ -0,0 +1,23 @@
+package com.aemreunal.exception.project;
+
+/*
+ ***************************
+ * Copyright (c) 2014      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * aemreunal@gmail.com     *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+public class ProjectNotFoundException extends NullPointerException {
+    public ProjectNotFoundException() {
+        super("The requested project can not be found.");
+    }
+}
