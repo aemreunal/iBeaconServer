6d4ba45d1550af937b8b45ff26671907f0685e63
A. Emre Ünal <aemreunal@gmail.com>
2014-08-14 16:30:13 +0300

    Refactor, add Spring Security configuration mapping
    Mapping not yet active!

--- src/main/java/com/aemreunal/config/WebAppInitializer.java
+++ src/main/java/com/aemreunal/config/WebAppInitializer.java
@@ -1,11 +1,14 @@
 package com.aemreunal.config;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /*
@@ -24,20 +27,28 @@ import org.springframework.web.servlet.DispatcherServlet;
  **************************
  */
 
-public class WebInitializer implements WebApplicationInitializer {
+public class WebAppInitializer implements WebApplicationInitializer {
     // TODO find out what this class is used for
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
-
-        webApplicationContext.register(CoreConfig.class, MVCConfig.class);
-        servletContext.addListener(new ContextLoaderListener(webApplicationContext));
+        AnnotationConfigWebApplicationContext webAppContext = new AnnotationConfigWebApplicationContext();
+//        webAppContext.register(CoreConfig.class, MVCConfig.class, SecurityConfig.class);
+        webAppContext.register(CoreConfig.class, MVCConfig.class);
+        servletContext.addListener(new ContextLoaderListener(webAppContext));
+//        servletContext.setInitParameter("defaultHtmlEscape", "true");
 
         AnnotationConfigWebApplicationContext dispatchCtx = new AnnotationConfigWebApplicationContext();
-
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatchCtx));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
+
+//        configureSpringSecurity(servletContext, webAppContext);
     }
+
+    private void configureSpringSecurity(ServletContext servletContext, WebApplicationContext rootContext) {
+        FilterRegistration.Dynamic springSecurity = servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy("springSecurityFilterChain", rootContext));
+        springSecurity.addMappingForUrlPatterns(null, true, "/*");
+    }
+
 }
