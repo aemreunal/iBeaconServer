87e9d99ccdac7578d7717af29064bb48610dc377
A. Emre Ünal <aemreunal@gmail.com>
2014-08-14 12:02:38 +0300

    Add custom exception

---
+++ src/main/java/com/aemreunal/domain/user/UsernameClashException.java
@@ -0,0 +1,24 @@
+package com.aemreunal.domain.user;
+
+/*
+ ***************************
+ * Copyright (c) 2014      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * aemreunal@gmail.com     *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+public class UsernameClashException extends IllegalArgumentException {
+
+    public UsernameClashException(String causingUsername) {
+        super("Username \'" + causingUsername + "\' is already taken. Please choose another username." );
+    }
+}
