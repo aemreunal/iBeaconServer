554586d5100d996b20439296cdfad0544cc427eb
A. Emre Ünal <aemreunal@gmail.com>
2014-08-15 16:34:02 +0300

    Comment-out implementation

--- src/main/java/com/aemreunal/config/SecurityConfig.java
+++ src/main/java/com/aemreunal/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.aemreunal.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -30,6 +28,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+//    @Autowired
+//    private BCryptPasswordEncoder encoder;
+
 //    @Override
 //    protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.inMemoryAuthentication()
@@ -44,29 +45,78 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //            .and()
 //            .httpBasic();
 //    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/**").hasRole("USER")
-            .anyRequest().anonymous()
-            .and()
-            .httpBasic();
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//            .antMatchers("/**").hasRole("USER")
+//            .anyRequest().anonymous()
 //            .and()
-//            .formLogin()
-        ;
-    }
+//            .httpBasic().and().userDetailsService(new UserDetailsServiceImpl());
+////            .and()
+////            .formLogin()
+//        ;
+////        new UsernamePasswordAuthenticationToken("test", "test");
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        /*auth.inMemoryAuthentication()
+//            .withUser("user")
+//            .password("password")
+//            .roles("USER")
+//            .and()
+//            .withUser("adminr")
+//            .password("password")
+//            .roles("ADMIN", "USER");*/
+////        auth.jdbcAuthentication().
+//        auth.userDetailsService(new UserDetailsServiceImpl()).passwordEncoder(encoder);
+//    }
+}
+/*
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("user")
-            .password("password")
-            .roles("USER")
-            .and()
-            .withUser("adminr")
-            .password("password")
-            .roles("ADMIN", "USER");
-//        auth.jdbcAuthentication().
+@Service("userDetailsService")
+class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService dao;
+    @Autowired
+    private Assembler   assembler;
+
+    @Transactional
+    public UserDetails loadUserByUsername(String username)
+    throws UsernameNotFoundException, DataAccessException {
+
+        com.aemreunal.domain.User user = dao.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return assembler.buildUserFromUserEntity(user);
     }
 }
+
+@Service("assembler")
+class Assembler {
+
+    @Transactional
+    public UserDetails buildUserFromUserEntity(com.aemreunal.domain.User userEntity) {
+        String username = userEntity.getUsername();
+        String password = userEntity.getPassword();
+        boolean enabled = true; // userEntity.isActive();
+        boolean accountNonExpired = true; // userEntity.isActive();
+        boolean credentialsNonExpired = true; // userEntity.isActive();
+        boolean accountNonLocked = true; // userEntity.isActive();
+
+        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+//        for (SecurityRoleEntity role : userEntity.getRoles()) {
+            authorities.add(*/
+/*new SimpleGrantedAuthority(role.getRoleName())*//*
+new SimpleGrantedAuthority("USER"));
+//        }
+
+        org.springframework.security.core.userdetails.User user = new User(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        return user;
+    }
+}
+*/
