bea7bd1e1cf17c4657df513363378fd8fb0ee590
A. Emre Ünal <aemreunal@gmail.com>
2014-08-19 16:05:08 +0300

    Fix project getter bug

--- src/test/java/com/aemreunal/domain/project/ProjectGetter.java
+++ src/test/java/com/aemreunal/domain/project/ProjectGetter.java
@@ -17,6 +17,7 @@ package com.aemreunal.domain.project;
  */
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import org.apache.http.HttpStatus;
 import com.aemreunal.domain.EntityGetter;
 import com.jayway.restassured.path.json.JsonPath;
@@ -25,8 +26,8 @@ public class ProjectGetter extends EntityGetter {
     public static ArrayList<ProjectInfo> getAllProjects(String username) {
         JsonPath responseJson = getEntity("/" + username + "/project");
         ArrayList<ProjectInfo> projects = new ArrayList<>();
-        for (JsonPath projectJson : responseJson.getList("", JsonPath.class)) {
-            projects.add(new ProjectInfo(projectJson, username, ""));
+        for (HashMap projectMap : responseJson.getList("", HashMap.class)) {
+            projects.add(new ProjectInfo(projectMap, username, ""));
         }
         return projects;
     }
