aed4f94bc67a8aa9231df1a955596afa160f777c
A. Emre Ünal <aemreunal@gmail.com>
2014-08-24 00:49:31 +0300

    Create Scenario repo

---
+++ src/main/java/com/aemreunal/repository/scenario/ScenarioRepo.java
@@ -0,0 +1,24 @@
+package com.aemreunal.repository.scenario;
+
+/*
+ ***************************
+ * Copyright (c) 2014      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * aemreunal@gmail.com     *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.CrudRepository;
+import com.aemreunal.domain.Scenario;
+
+public interface ScenarioRepo extends CrudRepository<Scenario, Long>, JpaSpecificationExecutor {
+}
