015fa373712d4cf2a064b12fab46b2f228b4a23c
A. Emre Ünal <aemreunal@gmail.com>
2014-08-28 18:11:52 -0400

    Add ScenarioController advice

---
+++ src/main/java/com/aemreunal/controller/scenario/ScenarioControllerAdvice.java
@@ -0,0 +1,63 @@
+package com.aemreunal.controller.scenario;
+
+/*
+ ***************************
+ * Copyright (c) 2014      *
+ *                         *
+ * This code belongs to:   *
+ *                         *
+ * @author Ahmet Emre Ünal *
+ * S001974                 *
+ *                         *
+ * aemreunal@gmail.com     *
+ * emre.unal@ozu.edu.tr    *
+ *                         *
+ * aemreunal.com           *
+ ***************************
+ */
+
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.aemreunal.exception.scenario.ScenarioNotFoundException;
+import com.aemreunal.helper.JsonBuilder;
+
+@ControllerAdvice
+public class ScenarioControllerAdvice {
+    @ExceptionHandler(ScenarioNotFoundException.class)
+    public ResponseEntity<JSONObject> scenarioNotFoundExceptionHandler(ScenarioNotFoundException ex) {
+        JSONObject responseBody = new JsonBuilder().add("reason", "scenario")
+                                                   .add("error", ex.getLocalizedMessage())
+                                                   .build();
+        return new ResponseEntity<JSONObject>(responseBody, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<JSONObject> constraintViolationExceptionHandler(ConstraintViolationException ex) {
+        JSONObject responseBody = new JsonBuilder().add("reason", "scenario")
+                                                   .add("error", "Constraint violation error ocurred! Unable to save scenario.")
+                                                   .add("violations", formatViolations(ex.getConstraintViolations()))
+                                                   .build();
+        return new ResponseEntity<JSONObject>(responseBody, HttpStatus.BAD_REQUEST);
+    }
+
+    private JSONArray formatViolations(Set<ConstraintViolation<?>> violations) {
+        JSONArray violationDescriptions = new JSONArray();
+        for (ConstraintViolation<?> violation : violations) {
+            Map<String, String> violationDescription = new HashMap<>();
+            violationDescription.put("property", violation.getPropertyPath().toString());
+            violationDescription.put("violation", violation.getMessage());
+            violationDescriptions.add(violationDescription);
+        }
+        return violationDescriptions;
+    }
+}
