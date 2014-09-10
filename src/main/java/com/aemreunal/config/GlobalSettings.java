6b7fc267614f6786fc8b4c10163d20f94ac236e3
A. Emre Ünal <aemreunal@gmail.com>
2014-09-10 00:36:31 -0700

    Whitespace

--- src/main/java/com/aemreunal/config/GlobalSettings.java
+++ src/main/java/com/aemreunal/config/GlobalSettings.java
@@ -65,9 +65,9 @@ public class GlobalSettings {
     public static final String BEACONGROUP_ADD_MEMBER_MAPPING    = BEACONGROUP_ID_MAPPING + "/addmember";
     public static final String BEACONGROUP_REMOVE_MEMBER_MAPPING = BEACONGROUP_ID_MAPPING + "/removemember";
     // Beacon
-    public static final String SCENARIO_PATH_MAPPING               = PROJECT_SPECIFIC_MAPPING + "/scenarios";
-    public static final String SCENARIO_ID_MAPPING                 = "/{scenarioId}";
-    public static final String SCENARIO_SPECIFIC_MAPPING           = SCENARIO_PATH_MAPPING + SCENARIO_ID_MAPPING;
+    public static final String SCENARIO_PATH_MAPPING             = PROJECT_SPECIFIC_MAPPING + "/scenarios";
+    public static final String SCENARIO_ID_MAPPING               = "/{scenarioId}";
+    public static final String SCENARIO_SPECIFIC_MAPPING         = SCENARIO_PATH_MAPPING + SCENARIO_ID_MAPPING;
 
 
     //-------------------------------------------------------------------------------------------
