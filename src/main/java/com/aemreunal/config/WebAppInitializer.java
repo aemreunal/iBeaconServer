c39e4531afbd361c3792c7d94ead60ff9b4824ec
A. Emre Ünal <aemreunal@gmail.com>
2014-10-04 01:44:15 +0300

    Remove clutter

--- src/main/java/com/aemreunal/config/WebAppInitializer.java
+++ src/main/java/com/aemreunal/config/WebAppInitializer.java
@@ -1,14 +1,11 @@
 package com.aemreunal.config;
 
-import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /*
@@ -33,22 +30,15 @@ public class WebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext webAppContext = new AnnotationConfigWebApplicationContext();
-//        webAppContext.register(CoreConfig.class, MVCConfig.class, SecurityConfig.class);
-        webAppContext.register(CoreConfig.class, MVCConfig.class);
+        webAppContext.register(CoreConfig.class, MVCConfig.class, SecurityConfig.class);
         servletContext.addListener(new ContextLoaderListener(webAppContext));
-//        servletContext.setInitParameter("defaultHtmlEscape", "true");
 
         AnnotationConfigWebApplicationContext dispatchCtx = new AnnotationConfigWebApplicationContext();
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(dispatchCtx));
+
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
-
-//        configureSpringSecurity(servletContext, webAppContext);
     }
 
-    private void configureSpringSecurity(ServletContext servletContext, WebApplicationContext rootContext) {
-        FilterRegistration.Dynamic springSecurity = servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy("springSecurityFilterChain", rootContext));
-        springSecurity.addMappingForUrlPatterns(null, true, "/*");
-    }
 
 }
